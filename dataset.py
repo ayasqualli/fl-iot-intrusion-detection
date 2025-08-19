a65ec3bb7ca66718a9fb4b0b5f93507a7a96aca0
Aya Squalli Houssaini <157663300+ayasqualli@users.noreply.github.com>
2025-08-19 02:12:32 +0100

    Initial Commit

---
+++ dataset.py
@@ -0,0 +1,22 @@
+# N-BaIoT Dataset loader
+
+CITATION = """@misc{Dua:2019 ,
+author = "Dua, Dheeru and Graff, Casey",
+year = "2017",
+title = "{UCI} Machine Learning Repository",
+url = "https://archive.ics.uci.edu/static/public/442/detection+of+iot+botnet+attacks+n+baiot.zip",
+institution = "University of California, Irvine, School of Information and Computer Sciences" }"""
+
+DEVICES_NAME = [
+    "Danmini_Doorbell",
+    "Ecobee_Thermostat",
+    "Ennio_Doorbell",
+    "Philips_B120N10_Baby_Monitor",
+    "Provision_PT_737E_Security_Camera",
+    "Provision_PT_838_Security_Camera",
+    "Samsung_SNH_1011_N_Webcam",
+    "SimpleHome_XCS7_1002_WHT_Security_Camera",
+    "SimpleHome_XCS7_1003_WHT_Security_Camera"
+]
+ATTACK_NAMES = ["benign", "combo", "junk", "mirai_ack", "mirai-udp", "mirai_updplain", "scan", "tcp", "udp"]
+
